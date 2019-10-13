a40a142fd675483249b9ff56f3aeaf5040252aaf
Sergio Panjoj <sergiopanjoj@gmail.com>
2019-10-13 16:08:02 -0700

    Make API call and populate table

--- codepath-flix-app/MovieViewController.swift
+++ codepath-flix-app/MovieViewController.swift
@@ -10,33 +10,48 @@ import UIKit
 
 class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    var movies = [[String:Any]]()
     @IBOutlet weak var table: UITableView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         table.delegate = self
         table.dataSource = self
+        getMovies()
     }
     
     // MARK: - TableView Methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "this is row \(indexPath.row)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        let movie = movies[indexPath.row]
+        cell.movieTitle.text = (movie["title"] as! String)
+        cell.movieSynopsis.text = (movie["overview"] as! String)
         return cell
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private Network Calls
+    private func getMovies() {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url,
+                                 cachePolicy: .reloadIgnoringLocalCacheData,
+                                 timeoutInterval: 10)
+        let session = URLSession(configuration: .default,
+                                 delegate: nil,
+                                 delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+            let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            self.movies = dataDictionary["results"] as! [[String:Any]]
+            self.table.reloadData()
+           }
+        }
+        task.resume()
     }
-    */
 
 }
